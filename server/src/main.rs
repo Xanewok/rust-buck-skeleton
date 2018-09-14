d70b11c9239c43e8c5e78afeb719189be7768a48
Igor Matuszewski <Xanewok@gmail.com>
2018-09-14 16:35:27 +0200

    Add simple structs and funcs

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,13 +3,35 @@ extern crate tokio;
 
 use tokio::prelude::*;
 
-fn main() {
-    let one = future::lazy(|| Ok(1));
-    let add = project1::addone(one);
-    let done = add.and_then(|v| {
-        println!("v={}", v);
-        Ok(())
-    });
+struct Struct;
+struct YetAnother;
 
-    tokio::run(done);
+type Res = Vec<u8>;
+
+fn another() -> Res {
+     Vec::new()
+}
+
+fn fetch_struct() -> Struct {
+     Struct
+}
+
+/// Some documentation.
+fn some_function() -> u32 {
+     42
+}
+
+fn main() {
+     let x = fetch_struct();
+     let a = 3;
+     let b = a + 2;
+
+     let one = future::lazy(|| Ok(1));
+     let add = project1::addone(one);
+     let done = add.and_then(|v| {
+          println!("v={}", v);
+          Ok(())
+     });
+
+     tokio::run(done);
 }
